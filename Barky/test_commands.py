ff0a17dbe7428e1f1fcb40c4556a4c7ffa95d549
Sushma <sushhpat@gmail.com>
2021-02-25 22:56:48 -0500

    update

--- Barky/test_commands.py
+++ Barky/test_commands.py
@@ -1,3 +1,21 @@
+import commands as commands
 import pytest
 
-import commands
+
+commands.CreateBookmarksTableCommand().execute()
+
+def test_Add_Bookmark_Commands():    
+    data ={'title': 'testbookmark',
+            'url': 'testbookmark.com',
+            'notes': 'this is for test'}
+
+    add = commands.AddBookmarkCommand().execute(data)
+    assert add =='Bookmark added!'
+
+def test_List_bookmark_Commands():
+  Blist = commands.ListBookmarksCommand().execute()[0]
+  assert Blist[1] == 'testbookmark'
+
+def test_Delete_Bookmark_Commands():
+    delete = commands.DeleteBookmarkCommand().execute('1')
+    assert delete == 'Bookmark deleted!'
